0f777f1c784b58a63c26ad2b7546b93e4a6ea1a1
sr <sr@computer-engineering.fr>
2024-03-16 11:05:12 +0100

    Add pyinstaller buildinf script

---
+++ pyinstaller/build_theme-editor.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+C:\Users\BaramoK\AppData\Local\Programs\Python\Python311\Scripts\pyinstaller ..\theme-editor.py --onefile --windowed --uac-admin --collect-data sv_ttk --hidden-import "babel.numbers"
